31925135d669ea51be2e3110e265dda289a6a492
euxzy <mhd.rijal203@gmail.com>
2025-05-01 20:03:35 +0700

    player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update() {
+        Vector2 inputVector = new(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new(inputVector.x, 0, inputVector.y);
+        transform.position += moveSpeed * Time.deltaTime * moveDirection;
+    }
+}
